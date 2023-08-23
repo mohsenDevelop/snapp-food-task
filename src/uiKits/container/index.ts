6441cdedb74d3282e5ea59680f7b47416ef83939
mohsen kermanifar <mohsen.kermanifar@gmail.com>
2023-08-23 23:11:44 +0330

    add Container component

---
+++ src/uiKits/container/index.ts
@@ -0,0 +1,11 @@
+'use client';
+
+import styled from 'styled-components';
+
+export const Container = styled.section`
+    padding-right: 16px;
+    padding-left: 16px;
+    margin-right: auto;
+    margin-left: auto;
+    width: 100%;
+`;
