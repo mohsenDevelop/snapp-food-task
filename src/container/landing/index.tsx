c8661ace16d334eb93d2a3c1570bc32f37989a42
mohsen kermanifar <mohsen.kermanifar@gmail.com>
2023-08-23 23:36:59 +0330

    add RestaurantPreview component

--- src/container/landing/index.tsx
+++ src/container/landing/index.tsx
@@ -1,13 +1,19 @@
 import { Container } from '@uiKits/container';
 import { VStack } from '@uiKits/stack';
+import RestaurantPreview from '@container/restaurant/preview';
 
 const HomeConatiner = () => {
     return (
         <Container>
-            <VStack>
-                Helooo
+            <VStack
+                style={{
+                    padding: '16px 0px',
+                    gap: '24px'
+                }}
+            >
+                <RestaurantPreview />
+                <RestaurantPreview />
             </VStack>
-
         </Container>
     );
 };
