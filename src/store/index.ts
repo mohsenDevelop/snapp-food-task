1aadc15409f4498362885102beca3b228aa7813d
mohsen kermanifar <mohsen.kermanifar@gmail.com>
2023-08-24 00:54:51 +0330

    fix store configuration

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,55 +1,19 @@
-import {
-    configureStore,
-    combineReducers
-} from '@reduxjs/toolkit';
-import {
-    persistReducer,
-    persistStore,
-    FLUSH,
-    REHYDRATE,
-    PAUSE,
-    PERSIST,
-    PURGE,
-    REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
+import { configureStore } from '@reduxjs/toolkit';
 
-import {
-    useSelector as useReduxSelector,
-    useDispatch as useReduxDispatch,
-    type TypedUseSelectorHook,
-} from 'react-redux';
-
+import { api } from '@api/client/api';
 import { vendorSlice } from '@slices/vendorSlide';
 import { rtkQueryErrorLogger } from '@config/ErrorMiddleware';
-import { api } from '@api/client/api';
-import persistMiddleware from '@config/persistMiddleware';
 
-const cartPersistConfig = {
-    key: 'cart',
-    storage,
-    stateReconciler: autoMergeLevel2
-};
-
-const rootReducer = combineReducers({
-    [api.reducerPath]: api.reducer,
-    [vendorSlice.name]: vendorSlice.reducer,
-    // [cartSlice.name]: persistReducer(cartPersistConfig, cartSlice.reducer)
-});
-
-export const reduxStore = configureStore({
-    reducer: rootReducer,
-    middleware: (getDefaultMiddleware: any) => {
-        return getDefaultMiddleware({
-            serializableCheck: {
-                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-            },
-        }).concat(api.middleware).concat(persistMiddleware).concat(rtkQueryErrorLogger);
+export const store = configureStore({
+    reducer: {
+        [api.reducerPath]: api.reducer,
+        [vendorSlice.name]: vendorSlice.reducer,
     },
+    middleware: (getDefaultMiddleware) => {
+        return getDefaultMiddleware().concat(api.middleware).concat(rtkQueryErrorLogger);
+    },
+    devTools: process.env.NODE_ENV === 'development'
 });
 
-export const persistor = persistStore(reduxStore);
-
-export type RootState = ReturnType<typeof reduxStore.getState>
-export type AppDispatch = typeof reduxStore.dispatch
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
