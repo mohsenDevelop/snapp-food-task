d2b354bd7d523e9d26462ea9c69d3e58162f2b48
mohsen kermanifar <mohsen.kermanifar@gmail.com>
2023-08-24 00:25:28 +0330

    fix store tyoe

--- src/store/index.ts
+++ src/store/index.ts
@@ -50,9 +50,6 @@ export const reduxStore = configureStore({
 });
 
 export const persistor = persistStore(reduxStore);
-export const useDispatch = () => useReduxDispatch<ReduxDispatch>();
-export const useSelector: TypedUseSelectorHook<ReduxState> = useReduxSelector;
 
-export type ReduxStore = typeof reduxStore;
-export type ReduxState = ReturnType<typeof reduxStore.getState>;
-export type ReduxDispatch = typeof reduxStore.dispatch;
+export type RootState = ReturnType<typeof reduxStore.getState>
+export type AppDispatch = typeof reduxStore.dispatch
