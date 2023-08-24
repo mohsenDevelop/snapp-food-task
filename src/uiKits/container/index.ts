36a7380b0d26ef488b35970973c0036e3a67f7ab
mohsen kermanifar <mohsen.kermanifar@gmail.com>
2023-08-24 15:13:24 +0330

    add media query

--- src/uiKits/container/index.ts
+++ src/uiKits/container/index.ts
@@ -3,9 +3,14 @@
 import styled from 'styled-components';
 
 export const Container = styled.section`
-    padding-right: 16px;
-    padding-left: 16px;
+    padding-right: 24px;
+    padding-left: 24px;
     margin-right: auto;
     margin-left: auto;
     width: 100%;
+    max-width: 700px;
+    @media (max-width: 700px) {
+        padding-right: 16px;
+        padding-left: 16px;
+    }
 `;
