0f3ceceeb83efac699dcd6db3b5158efae855f6c
mohsen kermanifar <mohsen.kermanifar@gmail.com>
2023-08-24 11:16:31 +0330

    add base url

--- src/api/client/api.ts
+++ src/api/client/api.ts
@@ -1,11 +1,11 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-import { _TOKEN_NAME } from '@config/urls';
+import { BASE_API_URL, _TOKEN_NAME } from '@config/urls';
 
 export const api = createApi({
     reducerPath: 'api',
     baseQuery: fetchBaseQuery({
-        baseUrl: `${process.env.NEXT_PUBLIC_API}/api/v1`,
+        baseUrl: BASE_API_URL,
         prepareHeaders: (headers: any) => {
             const token = localStorage.getItem(_TOKEN_NAME);
             if (token) {
